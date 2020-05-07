284e4416682928bcf1b6637a63ca298ef12595c3
Pavel Vesely <pavel@vesely.dev>
2020-05-07 09:53:44 +0200

    Update to Oqtane 0.9

--- Oqtane.Loader/Program.cs
+++ Oqtane.Loader/Program.cs
@@ -1,41 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Oqtane.Infrastructure;
 
 namespace Oqtane.Loader
 {
     public class Program
     {
-
         public static void Main(string[] args)
         {
             var host = BuildWebHost(args);
             using (var serviceScope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
-                var databaseManager = serviceScope.ServiceProvider.GetService<DatabaseManager>();
-                databaseManager.StartupMigration();
+                var databaseManager = serviceScope.ServiceProvider.GetService<IDatabaseManager>();
+                databaseManager.Install();
             }
+
             host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseConfiguration(new ConfigurationBuilder()
                     .AddCommandLine(args)
                     .Build())
                 .UseStartup<MyStartup>()
                 .Build();
+        }
     }
-    
+
     // important for webassembly and use own _Host.cshtml
     public class MyStartup : Startup
     {
